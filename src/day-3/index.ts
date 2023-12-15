51b6a737e7690735ae3703238181c6faca1d746f
Louis Boucquet <louisboucquet@gmail.com>
2023-12-15 21:45:41 +0100

    day-3 part 2

--- src/day-3/index.ts
+++ src/day-3/index.ts
@@ -3,52 +3,64 @@ import fs from 'fs';
 const symbolRegex = /[*@\-+#%=\/&$]/g;
 const symbolsChars = '*@-+#%=/&$';
 
-const sum = fs.readFileSync('./src/day-3/input')
+const lines = fs.readFileSync('./src/day-3/input')
 	.toString()
-	.split('\n')
-	.flatMap((line, lineNumber) =>
-		[...line.matchAll(/\d+/g)].map((number) =>
-			({
+	.split('\n');
+
+type Number = {
+	value: number,
+	lineNumber: number,
+	lineIndex: number,
+	numberLength: number,
+}
+
+type Symbol = {
+	value: string,
+	lineNumber: number,
+	lineIndex: number,
+}
+
+const numbers: Number[] = lines
+	.flatMap((line, lineNumber) => {
+		return [...line.matchAll(/\d+/g)].map((number) => {
+			return ({
 				value: parseInt(number[0]),
 				lineNumber,
 				lineIndex: number.index!,
 				numberLength: number[0].length,
-			})) ?? [])
-	.filter(number => {
-		const digitIndexes: number[] = [];
-		for (let i = 0; i < number.numberLength; i++) {
-			digitIndexes.push(i);
-		}
-		return digitIndexes
-			.some(digitIndex =>
-				[
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber - 1]?.[number.lineIndex + digitIndex - 1],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber + 1]?.[number.lineIndex + digitIndex - 1],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber - 1]?.[number.lineIndex + digitIndex + 1],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber + 1]?.[number.lineIndex + digitIndex + 1],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber - 1]?.[number.lineIndex + digitIndex],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber + 1]?.[number.lineIndex + digitIndex],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber]?.[number.lineIndex + digitIndex - 1],
-					(fs.readFileSync('./src/day-3/input')
-						.toString()
-						.split('\n'))[number.lineNumber]?.[number.lineIndex + digitIndex + 1],
-				].filter(char => char !== undefined).some(char => symbolsChars.includes(char)));
-	})
+			});
+		}) ?? [];
+	});
+
+const symbols: Symbol[] = lines
+	.flatMap((line, lineNumber) => {
+		return [...line.matchAll(symbolRegex)].map((number) => ({
+			value: number[0],
+			lineNumber,
+			lineIndex: number.index!,
+		})) ?? [];
+	});
+
+function isAdjacent(number: Number, symbol: Symbol): boolean {
+	for (let digitIndex = 0; digitIndex < number.numberLength; digitIndex++) {
+		if (Math.abs((number.lineIndex + digitIndex) - symbol.lineIndex) < 2 &&
+			Math.abs(number.lineNumber - symbol.lineNumber) < 2)
+			return true;
+	}
+
+	return false;
+}
+
+const part1 = numbers.filter(number => symbols.some(symbol => isAdjacent(number, symbol)))
 	.map(({value}) => value)
 	.reduce((a, b) => a + b);
 
-console.log(sum);
+const part2 =  symbols
+	.filter(symbol => symbol.value === "*")
+	.map(symbol => numbers.filter(number => isAdjacent(number, symbol)))
+	.filter(adjacentNumbers => adjacentNumbers.length === 2)
+	.map(([{value: v1}, {value: v2}]) => v1 * v2)
+	.reduce((a, b) => a + b);
+
+console.log(part1);
+console.log(part2);
